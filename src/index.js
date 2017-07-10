9f075e5c425005f5ff2b3f26a3bc1e8c9042e3f9
Hiroaki Yasutomo <yasutomo@shiftbrain.co.jp>
2017-07-10 17:09:59 +0900

    bugfix

--- src/index.js
+++ src/index.js
@@ -16,15 +16,9 @@ module.exports = (file, opt = {})=> {
     input += chunk
   }
   function end() {
-    let template
-
     try {
-      template = jade.compile(input, {filename: file})
-      this.queue(```
-        var React = require('react');
-        module.exports = ${template.toString()};
-        module.exports.locals = ${template.locals.toString()};
-      ```)
+      const template = jade.compile(input, {filename: file})
+      this.queue(`var React = require('react');\nmodule.exports = ${template.toString()};\nmodule.exports.locals = ${template.locals.toString()};`)
       this.queue(null)
     } catch (err) {
       err.stack = ''
